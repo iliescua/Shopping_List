f1ec9345cc73a32ff5927927ef2efbaf09f02dca
Andrew Iliescu <iliescuandrew@gmail.com>
2021-07-21 14:11:05 -0500

    Made the list items clickable so that they can have a strikethrough if ocmplete and added functionality so that the x button removes only that list item while the clear button removes all items from the list. Webiste is not complete functionally

--- script.js
+++ script.js
@@ -4,14 +4,21 @@ var btn_clear = document.getElementById("clear_btn");
 var btn_remove = document.getElementById("removeLast_btn");
 var itemContainer = document.getElementById("itemContainer");
 
-// Basic function to check and add an item to the ul
+// Basic function to check and add an item to the item container
 function addListItem() {
+    var itemContents = document.createElement("div");
     var paragraph = document.createElement("p");
     var btn_del = document.createElement("button");
+    // Add item contents div to the item container
+    itemContents.classList.add("itemContents");
+    itemContainer.appendChild(itemContents);
+    // Set item and button text
     paragraph.appendChild(document.createTextNode(input.value));
     btn_del.appendChild(document.createTextNode("X"));
     btn_del.id = "delete_btn";
-    itemContainer.append(paragraph, btn_del);
+    // Add item and button to the item contents div
+    itemContents.append(paragraph, btn_del);
+    // Clear user textfield
     input.value = "";
 }
 
@@ -29,14 +36,22 @@ input.addEventListener("keypress", function(event) {
     }
 });
 
-btn_del.addEventListener("click", function(){
-    
+// Action to strikethrough any selected item
+itemContainer.addEventListener("click", function(item){
+    if (item.target.tagName === "P") {
+        item.target.classList.toggle("complete");
+    }
+});
+
+// Action to delete row once user decides it is no londer necessary
+itemContainer.addEventListener("click", function(delClick){
+    if(delClick.target.id === "delete_btn")
+    delClick.target.parentElement.remove();
 });
 
 // Action to clear all entries in the list
 btn_clear.addEventListener("click", function() {
-    var element =  itemContainer.getElementsByTagName("p");
-    while(itemContainer.getElementsByTagName("p").length > 0) {
-        listStorage.removeChild(element[0]);
+    while(itemContainer.getElementsByClassName("itemContents").length > 0){
+        itemContainer.removeChild(itemContainer.firstChild);
     }
 });
